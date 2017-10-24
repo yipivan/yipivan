6bf6803acd264974cae9a3b685ad1f396d89cd0b
Ivan Yip <ivan_yip@msn.com>
2017-10-25 03:01:51 +0800

    individual assignment

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var scroll = new SmoothScroll('a[href*="#"]');
+    var options = {
+        strings: ["My name is Ivan, a web developer.", "I build mobile apps and websites", "Please feel free to take a look on my project", "Have a good day"],
+        typeSpeed: 40
+      }
+    var typed = new Typed(".typed", options);
+    $(document).on('scroll', function (e) {
+        $('.navbar').css('opacity', ($(document).scrollTop() / 1000));
+    });
+})
+
